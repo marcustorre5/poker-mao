9b0d2600423d191ac5141882f65ab20d0675f8a9
marcustorre5 <49458352+marcustorre5@users.noreply.github.com>
2021-12-01 20:48:44 -0300

    Add files via upload

---
+++ poker-mao.py
@@ -0,0 +1,383 @@
+print('♣♣♥♥ POKER HANDS ♦♦♠♠\n')
+
+def creditosini():
+    print('Desenvolvido para iniciados em poker')
+    print('BY Marcus Torres')
+    print('@marcustorre5\n')
+creditosini()
+while True:
+    quest = (input('Qual é a sua mão ?\n')).upper()
+
+    if quest == 'AA':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'KK':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'QQ':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'JJ':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == '1010':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == '99':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == '88':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == '77':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'AK':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'AQ':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'Aj':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'A10':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'KQ':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'KJ':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'K10':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'QJ':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'Q10':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'J10':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == 'J9':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == '109':
+        print(quest)
+        print('Sua mão serve para jogar em qualquer posição')
+
+    if quest == '66':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == '55':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'A9':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'A8':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'A7':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'A6':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'K9':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'Q9':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'Q8':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == 'J8':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == '108':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == '98':
+        print(quest)
+        print('Com sua mão jogue nas posições finais')
+
+    if quest == '44':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '33':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '22':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'A5':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'A4':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'A3':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'A2':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as Atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'K8':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'K7':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'K6':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'K5':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'K4':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'K3':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'K2':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'J7':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '107':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '97':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '96':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '87':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '86':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '76':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '75':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '65':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == '54':
+        print(quest)
+        print('Jogue nas posiçoes finais e observe as atitudes dos jogadores, pode ser que seja uma boa')
+
+    if quest == 'Q7':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'Q6':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'Q5':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'Q4':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'Q3':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'Q2':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'J6':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'J5':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'J4':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'J3':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == 'J2':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '106':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '105':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '104':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '103':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '102':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '95':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '94':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '93':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '92':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '85':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '84':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '83':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '82':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '74':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '73':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '72':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '64':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '63':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '62':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '53':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '52':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '43':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '42':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+    if quest == '32':
+        print(quest)
+        print('Essa mão é injogavél,')
+
+
+    if quest =='++':
+        break
+
+def creditosfinal():
+    print('tanks for using ☺')
+    print('BY Marcus Torres')
+    print('@marcustorre5')
+creditosfinal()
